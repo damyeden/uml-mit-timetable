64321c49064adb6a976e96658fd3b0beaa42ccc5
damyeden <teh01243@gmail.com>
2025-06-13 17:17:14 +0300

    Update class diagram

--- src/lib/models/Ecue.ts
+++ src/lib/models/Ecue.ts
@@ -1,3 +1,6 @@
+import prisma from "../prisma";
+import { Professor } from "./Professor";
+
 export class Ecue {
   private ecueId: number;
   public name: string;
@@ -14,5 +17,38 @@ export class Ecue {
   }
   public setEcueId(ecueId: number) {
     this.ecueId = ecueId;
+  }
+
+  public async getProfessors(): Promise<Professor[] | string | null> {
+    try {
+      const res = await prisma.enseignement.findMany({
+        where: {
+          ecueId: this.ecueId,
+        },
+        include: {
+          professor: {
+            include: {
+              person: true,
+            },
+          },
+        },
+      });
+
+      return res.map((r) => {
+        const { professor } = r;
+        return new Professor(
+          professor.professorId,
+          professor.personId,
+          professor.person.lastname,
+          professor.person.userId,
+          professor.person.lastname
+        );
+      });
+    } catch (error) {
+      if (error instanceof Error) {
+        return error.message;
+      }
+      return null;
+    }
   }
 }
